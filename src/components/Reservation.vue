74a72ad79fc10be4aef19b2e7539937c3ae14ade
Levina <brafalevina06@gmail.com>
2024-04-26 17:05:03 +0700

    Login Page, Register Page, Home Page(Filtering), dan Reservation Page form

--- src/components/Reservation.vue
+++ src/components/Reservation.vue
@@ -7,27 +7,22 @@
           <div class="form-container">
             <form @submit.prevent="submitForm" class="reservation-form">
               <div class="form-group">
-                <label for="address" class="form-label">Address:</label>
-                <input type="text" id="address" v-model="formData.address" class="form-control" required>
-              </div>
-              <div class="form-group">
-                <label for="age" class="form-label">Age:</label>
-                <input type="number" id="age" v-model="formData.age" class="form-control" required>
-              </div>
-              <div class="form-group">
-                <label for="weight" class="form-label">Weight:</label>
-                <input type="number" id="weight" v-model="formData.weight" class="form-control" required>
-              </div>
-              <div class="form-group">
-                <label for="bloodType" class="form-label">Blood Type:</label>
-                <select v-model="formData.bloodType" class="form-select" required>
-                  <option disabled value="">Please select blood type</option>
-                  <option value="A">A</option>
-                  <option value="B">B</option>
-                  <option value="O">O</option>
-                  <option value="AB">AB</option>
-                </select>
-              </div>
+                  <label for="address" class="form-label">Address:</label>
+                  <input type="text" id="address" v-model="formData.address" class="form-control" placeholder="Enter your address" required>
+                </div>
+                <div class="form-group">
+                  <label for="age" class="form-label">Age:</label>
+                  <input type="number" id="age" v-model="formData.age" class="form-control" placeholder="Enter your age" required>
+                </div>
+                <div class="form-group">
+                  <label for="weight" class="form-label">Weight:</label>
+                  <input type="number" id="weight" v-model="formData.weight" class="form-control" placeholder="Enter your weight" required>
+                </div>
+                <div class="form-group">
+                  <label for="bloodType" class="form-label">Blood Type:</label>
+                  <input type="text" id="bloodtype" v-model="formData.bloodtype" class="form-control" placeholder="Enter your blood type" required>
+                </div>
+
               <button type="submit" class="btn btn-primary">Submit</button>
             </form>
           </div>
